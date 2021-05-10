ec821c63b95e15c34b4fcf4b0fdcc5629e4483cd
olena.mordas <olena.mordas@synpulse.com>
2021-05-10 13:53:32 +0200

    Initial commit & generated api with OpenAPI Maven plugin

---
+++ src/main/java/com/example/kafka_challenge_3a/KafkaChallenge3aApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafka_challenge_3a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaChallenge3aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaChallenge3aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafka_challenge_3a/KafkaChallenge3aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka_challenge_3a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaChallenge3aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
