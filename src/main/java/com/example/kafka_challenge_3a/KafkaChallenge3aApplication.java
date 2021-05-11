30c55a4fc75f76e0012968ec3aeed581b48d810b
olena.mordas <olena.mordas@synpulse.com>
2021-05-11 10:25:44 +0200

    fixed package

--- src/main/java/com/example/kafka_challenge_3a/KafkaChallenge3aApplication.java
+++ src/main/java/com/example/kafka_challenge_3a/KafkaChallenge3aApplication.java
@@ -2,8 +2,10 @@ package com.example.kafka_challenge_3a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.example.kafka_challenge_3a", "io.reflectoring.model"})
 public class KafkaChallenge3aApplication {
 
 	public static void main(String[] args) {
